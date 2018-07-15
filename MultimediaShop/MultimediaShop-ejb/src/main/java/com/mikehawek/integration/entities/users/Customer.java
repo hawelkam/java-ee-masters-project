a487c823452194cae4f7d9bc62f6ae1054dcec76
hawelka <m.hawelka@gmail.com>
2018-07-15 16:41:05 +0200

    Basket removal

--- MultimediaShop/MultimediaShop-ejb/src/main/java/com/mikehawek/integration/entities/users/Customer.java
+++ MultimediaShop/MultimediaShop-ejb/src/main/java/com/mikehawek/integration/entities/users/Customer.java
@@ -12,16 +12,11 @@ import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.mikehawek.integration.entities.Basket;
 import com.mikehawek.integration.entities.Order;
 
 @Entity
 @DiscriminatorValue("Customer")
 public class Customer extends User{
-    @OneToOne(fetch = FetchType.LAZY, cascade=CascadeType.ALL,
-            mappedBy = "customer")
-    private Basket basket;
-
     @OneToMany(cascade = CascadeType.ALL)
     @JoinColumn(name = "ORDER_ID")
     private List<Order> orders = new ArrayList<>();
